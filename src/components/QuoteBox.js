18e14029ba5e7f5086b6f4c11bf257c7fb58c08e
sh3yk0 <sh3yk0@gmail.com>
2021-06-27 15:16:40 -0500

    se eliminaron todas las pruebas en consola

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -4,14 +4,12 @@ import QuoteInfo from './QuoteInfo';
 import HeaderQuote from './HeaderQuote'
 import FooterQuote from './FooterQuote'
 const QuoteBox = () => {
+    //random number for the position in the data json
     const minNum = 0;
     const maxNum = data.quotes.length;
     let random = Math.floor(Math.random() * (maxNum - minNum)) + minNum
     //initial state
     const [quote, setQuote] = useState(data.quotes[random]);
-    console.log(data.quotes[random])
-    console.log(data.quotes)
-    console.log(random)
     return(
         <div className='quote-machine'>
             <HeaderQuote/>
@@ -21,7 +19,6 @@ const QuoteBox = () => {
                     <button className='quote-button' onClick={() => {
                         random= Math.floor(Math.random() * (maxNum - minNum)) + minNum
                         setQuote(data.quotes[random])
-                        console.log(data.quotes[random])
                         }}>Change Quote</button>
                 </div>
             </main>
